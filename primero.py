61247f3c86861b7d3d58b2b9f1ef83f47070b04b
daferarte <daferarte@gmail.com>
2025-03-21 19:28:22 -0500

    primero

---
+++ primero.py
@@ -0,0 +1,9 @@
+nombre = "Juan" # Tipo str (cadena de caracteres)
+edad = 25 # Tipo int(entero)
+altura = 1.75 # Tipo float(decimal)
+es_estudiante = True # Tipo bool (booleano => True or False)
+
+print(type(nombre)) # <class 'str'>
+print(type(edad)) # <class 'int'>
+print(type(altura)) # <class 'float'>
+print(type(es_estudiante)) # <class 'bool'>
