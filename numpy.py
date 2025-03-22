c23ff5dc5e1aea9e2cf17163089f9d82405916aa
daferarte <daferarte@gmail.com>
2025-03-21 20:35:22 -0500

    numpy

---
+++ numpy.py
@@ -0,0 +1,29 @@
+################################################################
+# Numpy
+################################################################
+
+################################
+# Importando - pip install numpy - python -m pip install numpy - py -m pip install numpy
+################################
+import numpy as np
+
+################################
+# Creacion de un arreglo con numpy
+################################
+arr = np.array([1, 2, 3, 4, 5])
+print (arr)
+
+################################
+# operaciones con un arreglo
+################################
+arr2 = arr * 2 # Multiplica cada elemento por 2
+print (arr2)
+
+################################
+# Propiedades de los arreglos
+################################
+
+print (arr.shape) # Mostrar la forma del arreglo
+print (arr.size) # Numero total de elementos
+print (arr.dtype) # Tipo de dato almacenado
+
