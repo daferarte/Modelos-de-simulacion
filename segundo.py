739c6e10bca8d6f58e1fcdbe65eb09f373c19fab
daferarte <daferarte@gmail.com>
2025-03-21 19:57:09 -0500

    Condicionales

---
+++ segundo.py
@@ -0,0 +1,17 @@
+################################################################
+# Estructuras de control
+################################################################
+
+################################
+# Condicionales
+################################
+
+edad = 18
+
+if edad >= 18:
+    print("Eres mayor de edad")
+elif edad == 17:
+    print("Casi eres mayor de edad")
+else: 
+    print("Eres menor de edad")
+    
