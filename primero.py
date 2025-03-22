aaf4531b683efe1e26e0600b1506f75f2301380e
daferarte <daferarte@gmail.com>
2025-03-21 19:36:14 -0500

    conversion de tipos

--- primero.py
+++ primero.py
@@ -3,7 +3,21 @@ edad = 25 # Tipo int(entero)
 altura = 1.75 # Tipo float(decimal)
 es_estudiante = True # Tipo bool (booleano => True or False)
 
+################################
+# Conocer el tipo de variable
+################################
+
 print(type(nombre)) # <class 'str'>
 print(type(edad)) # <class 'int'>
 print(type(altura)) # <class 'float'>
 print(type(es_estudiante)) # <class 'bool'>
+
+################################
+# Conversion de tipo
+################################
+
+numero = "10"
+numero_entero = int(numero) # Convierto a entero
+numero_flotante = float(numero) # Conversion a flotante
+print(numero_entero, numero_flotante)
+
