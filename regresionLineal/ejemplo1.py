1477c1f92dcd187988029206c2ebb169ead5fa21
daferarte <daferarte@gmail.com>
2025-03-28 19:32:25 -0500

    regresion lineal simple 1

--- regresionLineal/ejemplo1.py
+++ regresionLineal/ejemplo1.py
@@ -1,5 +1,6 @@
+# importar librerias
 import numpy as np 
-import matplotlib.pyplot as plb
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression 
 
 # Datos simulados
@@ -8,3 +9,13 @@ Y = np.array([5000,  7000, 8500, 9200, 11000]) # Ventas
 
 # Modelo de regresion
 
+modelo = LinearRegression()
+modelo.fit(X,Y)
+
+# Prediccion y visualizacion
+plt.scatter(X,Y, color='blue', label='Datos reales')
+plt.plot(X, modelo.predict(X), color='red', label='Regresion lineal')
+plt.xlabel('Inversion en publicidad $')
+plt.ylabel('Ventas $')
+plt.legend()
+plt.show()
