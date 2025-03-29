9998f5ffcdf38edabb9d7c337f8b0f3c4cfaedf4
daferarte <daferarte@gmail.com>
2025-03-28 19:22:54 -0500

    modelo de gresion lineal 1

---
+++ regresionLineal/ejemplo1.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import matplotlib.pyplot as plb
+from sklearn.linear_model import LinearRegression 
+
+# Datos simulados
+X = np.array([1000, 2000, 3000, 4000, 5000]).reshape(-1, 1) # inversion en publicidad
+Y = np.array([5000,  7000, 8500, 9200, 11000]) # Ventas
+
+# Modelo de regresion
+
